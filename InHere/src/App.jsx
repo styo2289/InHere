e5263c791a8c2e8a501154939e555c72f64e7707
Summer <summertyo@yahoo.com>
2024-03-09 11:20:14 -0600

    First Commit

--- InHere/src/App.jsx
+++ InHere/src/App.jsx
@@ -4,22 +4,30 @@ import {
     Route
 } from "react-router-dom";
 import "./App.css";
-import SideNavBar from "./components/SideNavBar.jsx";
+import SideNavBar from "./components/SideNavBar/SideNavBar.jsx";
 
 import HomePage from "./pages/HomePage.jsx";
 import TaskTrackerPage from "./pages/TaskTrackerPage.jsx";
 import EventStatsPage from "./pages/EventStatsPage.jsx";
+import Login from "./components/Login/Login.jsx";
+
 
 function App() {
     return (
+
+
         <div className="App">
-            <SideNavBar/>
+            {/* <SideNavBar/> */}
             <main>
-                <Routes>
-                    <Route path="/" element={ <HomePage/> }/>
+                { /*All NavSideBar is commented out to be able to display the Login for now.*/}
+                <Login/>
+
+                {/* <Routes>
+                    <Route path="/" element={ <Login/> }/>
+                    <Route path="/homePage" element={ <HomePage/> }/>
                     <Route path="/taskTracker" element={ <TaskTrackerPage/> }/>
                     <Route path="/eventStats" element={ <EventStatsPage/> }/>
-                </Routes>
+                </Routes> */}
             </main>
         </div>
     )
---
+++ InHere/src/components/Login/Login.jsx
@@ -0,0 +1,23 @@
+import styles from "./Login.module.css"
+
+function Login (){
+    return (
+        <div className='wrapper'>
+            <form action="">
+                <h1>Login</h1>
+                <div className="input">
+                    <input type="text" placeholder="Username" required/>
+                </div>
+                <div className="input">
+                    <input type="text" placeholder="Password" required/>
+                </div>
+
+                <div className='registerLink'>
+                    <p>First time in here? <a href="">Sign Up!</a></p>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Login
