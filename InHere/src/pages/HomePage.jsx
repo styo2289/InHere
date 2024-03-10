19337575936643d262c1d01cbdcf880934cdbec1
Gabriel <goldferret1@gmail.com>
2024-03-10 11:56:07 -0500

    Created a basic skeleton HomePage * Section for Header * Grid Background * Basic items for visualization

--- InHere/src/pages/HomePage.jsx
+++ InHere/src/pages/HomePage.jsx
@@ -1,7 +1,19 @@
+import "./HomePage.css"
+
 function HomePage () {
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className="pageLayout">
+            <h1>HomePage</h1>
+            <span className="gridBackground">
+                <div className="pageContent">
+                    <div className="item item-1">Item 1</div>
+                    <div className="item item-2">Item 2</div>
+                    <div className="item item-3">Item 3</div>
+                    <div className="item item-4">Item 4</div>
+                    <div className="item item-1">Item 5</div>
+                    <div className="item item-2">Item 6</div>
+                </div>
+            </span>
         </div>
     )
 }
